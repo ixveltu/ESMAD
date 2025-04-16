22d379d0fa78c4c46f735bbdaf34a8ef6e850bbf
Emanuel Gomes <emanuel.jose.f.gomes@gmail.com>
2025-04-16 21:02:55 +0100

    teste

---
+++ Teste/script.js
@@ -0,0 +1,48 @@
+let nomeJogador = prompt("Qual o seu nome?");
+let tempo = 0, timer;
+let respondidas = 0;
+
+
+
+function mostrarPromptJogador() {
+  
+}
+
+function criarGrelha() {
+ 
+}
+
+function mostrarPergunta(categoria, celula) {
+ 
+}
+
+function validarResposta(resposta, pergunta, celula) {
+ 
+}
+
+function iniciarContador() {
+  timer = setInterval(() => {
+    tempo++;
+    document.getElementById("contador").textContent = `Tempo: ${tempo}s`;
+  }, 1000);
+}
+
+function registarTempo() {
+ 
+}
+
+function atualizarLeaderboard() {
+  
+}
+
+function atualizarTabelaAdmin() {
+ 
+}
+
+document.getElementById("btnAdicionarPergunta").addEventListener("click", adicionarPergunta)
+
+function adicionarPergunta() {
+  
+}
+
+window.onload = mostrarPromptJogador;
