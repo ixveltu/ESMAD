d9998c2209732e4915f9a45f3dc1cb9c43417f28
Emanuel Gomes <emanuel.jose.f.gomes@gmail.com>
2025-11-03 16:57:58 +0000

    3/11/25

--- 2Ano/CG/Projeto/js/opcoes.js
+++ 2Ano/CG/Projeto/js/opcoes.js
@@ -1,13 +1,1 @@
-let initialTime = 30; // tempo inicial em segundos
 
-function getInitialTime() {
-  return initialTime;
-}
-
-// aumenta o tempo em +30 segundos
-function add30Seconds() {
-  initialTime += 30;
-  alert("Tempo aumentado! Novo tempo: " + initialTime + " segundos");
-}
-
-//chamar add30Seconds() a partir de um botão nas opções.html
