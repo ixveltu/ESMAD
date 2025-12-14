6f0e965a8564096a4a09dc3ab8c63f954c59fa16
Emanuel Gomes <emanuel.jose.f.gomes@gmail.com>
2025-12-14 03:14:09 +0000

    gg

--- 2Ano/PWI/Exercicios/Ficha3/EX3/src/component/MovieReview.vue
+++ 2Ano/PWI/Exercicios/Ficha3/EX3/src/component/MovieReview.vue
@@ -1,17 +1,30 @@
 <template>
  <div>
-
+  <h1><strong> Title: </strong> {{ title }}</h1>
+  <p><strong> Description: </strong> {{ description }} </p>
+  <RatingStars @rating-changed="ratingChanged" />
  </div>
 </template>
 
 <script>
+import RatingStars from '@/component/RatingStars.vue';
+
  export default {
+
   data() {
-   movie: {
-    nome: "Dune",
+   movies: [
+    {
+    title: "Dune",
     description: "Uma saga épica de poder, destino e sobrevivência num planeta desértico chamado Arrakis."
    }
-  }
+  ]
+  },
+  
+  methods: {
+   ratingChanged(rat) {
+    console.log('Classificação escolhida:', rat);
+   }
+  } 
  }
 </script>
 
